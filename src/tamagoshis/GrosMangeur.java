6cc6b39a2116a0b6abb317b4f02b5db7f3a5f54a
julien <julien-34@hotmail.fr>
2015-11-05 11:36:11 +0100

    Correction des packages

---
+++ src/tamagoshis/GrosMangeur.java
@@ -0,0 +1,10 @@
+package tamagoshis;
+
+public class GrosMangeur extends Tamagoshi {
+
+	public GrosMangeur(String chaine) {
+		super(chaine);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/util/Utilisateur.java
+++ src/util/Utilisateur.java
@@ -1,4 +1,4 @@
-package jeu;
+package util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
